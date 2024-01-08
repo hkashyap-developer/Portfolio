d653ee3b08502dd5eee3c41f459aa27ba141cd54
Himanshu Kashyap <hkashyap.developer@gmail.com>
2024-01-08 08:49:29 +0530

    React Router setup

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Home from "../src/Pages/Home/home.jsx";
+import About from "../src/Pages/About/about.jsx";
+import Contact from "../src/Pages/Contact/contact.jsx";
+
+import { BrowserRouter, Routes, Route, Link} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="fullPageWrapper">
+          <div class="menuWrap">
+              <nav>
+                <ul>
+                  <li><Link to="">Home</Link></li>
+                  <li><Link to="/about">About</Link></li>
+                  <li><Link to="/contact">Contact</Link></li>            
+                </ul>
+              </nav> 
+            </div>
+            <div className="content">
+              <Routes>
+                <Route path="" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </div>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
